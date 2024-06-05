77f6dd16b9099c72b8e290ea4ab1d63822d7b796
Bo zhang <bo.zhang@pku.edu.cn>
2024-06-04 17:38:31 -0700

    Add files via upload

---
+++ 0_preparation/bash_nonbrain_tissue_remove_BET.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+bet "/Users/bo/Documents/data_dean_lab/sarah_safetyEstimationTask/sub-001/sub-001_T1w.nii.gz" "/Users/bo/Documents/data_dean_lab/sarah_safetyEstimationTask/sub-001/sub-001_T1w_brain" -S -B
+
+
+
