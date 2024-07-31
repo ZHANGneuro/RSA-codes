c8da695e706a0dd7b67347e8ec75e4c1cf620ea3
Bo zhang <bo.zhang@pku.edu.cn>
2024-07-31 00:46:19 -0700

    Delete 0_preparation directory

--- 0_preparation/bash_nonbrain_tissue_remove_BET.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-bet "/Users/bo/Documents/data_dean_lab/sarah_safetyEstimationTask/sub-001/sub-001_T1w.nii.gz" "/Users/bo/Documents/data_dean_lab/sarah_safetyEstimationTask/sub-001/sub-001_T1w_brain" -S -B
-
-
-
--- 0_preparation/matlab_dcm_to_nii.m
+++
@@ -1,6 +0,0 @@
-
-
-
-folder_of_dicoms = '/Users/bo/Documents/data_dean_lab/NSFG_2/anat/1.3.12.2.1107.5.2.43.167050.2023040612203891436861730.0.0.0.dicom';
-folder_of_output = '/Users/bo/Documents/data_dean_lab/NSFG_2/anat';
-dicm2nii(folder_of_dicoms, folder_of_output, '.nii')
